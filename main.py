dc76a40d28d17368f132eedda4156e011bb5509f
Gergo Schonhardt <48925940+gesch97@users.noreply.github.com>
2025-01-29 13:05:26 +0100

    can read print area now

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import argparse
 import logging
+import os
+from dataclasses import dataclass
+
+
+@dataclass()
+class PrintArea:
+    X: int = -1
+    Y: int = -1
+    W: int = -1
+    H: int = -1
 
 
 def init_logging():
-    logging.basicConfig(filename="loc_randomizer.log", level=logging.INFO)
+    file_location = os.path.dirname(__file__)
+    file_name = "PrusaSlicer_loc_randomizer.log"
+    log_file_path = os.path.join(file_location, file_name)
+    logging.basicConfig(filename=log_file_path, level=logging.INFO)
     logging.getLogger().addHandler(logging.StreamHandler())
     logging.info("Logging starts")
+
+
+def offset_line(line, offset_x, offset_y):
+    pass
 
 
 def main():
@@ -17,6 +34,36 @@ def main():
     args = parser.parse_args()
     path = vars(args)["Path"]
     logging.info(f"The following path was provided: {path}")
+
+    with open(path) as f:
+        lines = f.read().split("\n")
+    # TODO: randomize xy
+    offset_x = 10
+    offset_y = 20
+
+    logging.info(f"Random offset (x,y): ({offset_x}, {offset_y})")
+
+    # find out which points are in the print area and which are not
+    mfff_line = next(filter(lambda line: line.startswith("M555"), lines))
+    bed_area = PrintArea()
+    for word in mfff_line.split(" "):
+        match word[0]:
+            case "X":
+                bed_area.X = int(word[1:])
+            case "Y":
+                bed_area.Y = int(word[1:])
+            case "W":
+                bed_area.W = int(word[1:])
+            case "H":
+                bed_area.H = int(word[1:])
+
+    logging.info(f"old print area: {bed_area}")
+
+    for i, line in enumerate(lines):
+        words = line.split(" ")
+        match words[0]:
+            case "M555":
+                lines[i] = offset_line(line, offset_x, offset_y)
 
 
 if __name__ == "__main__":
